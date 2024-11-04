c5ce3c4a2fa2ee24975bfecde5f6c834a122543f
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-11-03 20:20:34 -0800

    build(client): specify outDir to server's dist/public

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,8 +1,12 @@
-import { resolve } from 'node:path';
+import { join, resolve } from 'node:path';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    build: {
+        emptyOutDir: true,
+        outDir: join(__dirname, '..', 'server', 'dist', 'public'),
+    },
     plugins: [react()],
     resolve: {
         alias: {
