f16f06522a4db035f8644cdd48b40e4973643e41
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-11-03 21:28:22 -0800

    update(server): remove static file handler

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,18 +1,12 @@
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import cors from 'cors';
 import express from 'express';
 import { apiRouter } from '@/routes/api';
 import { env } from '@/utilities/env';
 
-const path = dirname(fileURLToPath(import.meta.url));
-
 const app = express();
 const port = env.PORT;
 
 app.use(cors());
-
-app.use(express.static(join(path, '..', 'public')));
 
 app.use('/api', apiRouter);
 
