4cb8549cb0ab1997a8287046e21253b32ca4973c
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-11-03 17:17:10 -0800

    refactor: rename App to be lowercase

--- packages/client/__tests__/app.test.tsx
+++ packages/client/__tests__/app.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import { describe, it } from 'vitest';
-import { App } from '@/App';
+import { App } from '@/app';
 
 describe('App', () => {
     it('renders the App component', () => {
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,8 +2,7 @@ import '@/styles/index.scss';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
-import { App } from './App';
+import { App } from '@/app';
 
 const rootElement = document.getElementById('root');
 
