0edf263ff18e842322116853d22afcae066f9200
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-11-03 19:04:51 -0800

    update(client): proxy /api requests in vite dev server

--- packages/client/src/routes/root.tsx
+++ packages/client/src/routes/root.tsx
@@ -1,11 +1,30 @@
-import { type ReactNode } from 'react';
+import { type ReactNode, useEffect, useState } from 'react';
 
 export function Root(): ReactNode {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        async function getMessage() {
+            const response = await fetch('/api/hello');
+            const data = await response.json();
+
+            console.log(data);
+
+            setMessage(data?.message);
+        }
+
+        void getMessage().catch(() => console.log('error in server'));
+    }, []);
+
     return (
-        <div className="min-h-screen flex">
-            <div className="flex-1 flex flex-col items-center">
+        <div className="min-h-screen flex flex-col">
+            <div className="flex flex-col items-center">
                 <h1 className="text-4xl font-semibold">React Template</h1>
             </div>
+            <p className="flex gap-2">
+                <span>From Server: </span>
+                <span>{message}</span>
+            </p>
         </div>
     );
 }
