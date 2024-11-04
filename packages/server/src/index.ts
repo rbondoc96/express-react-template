5fecbe8b46a6ed1e834f36572284b2df99b53fdc
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-11-03 20:21:36 -0800

    build(server): serve build static files from public dir

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,17 +1,20 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import cors from 'cors';
 import express from 'express';
 import { apiRouter } from '@/routes/api';
 import { env } from '@/utilities/env';
 
+const path = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = env.SERVER_PORT;
 
 app.use(cors());
-app.use('/api', apiRouter);
 
-app.get('/', (_req, res) => {
-    res.send('Express + TypeScript server');
-});
+app.use(express.static(join(path, '..', 'public')));
+
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
