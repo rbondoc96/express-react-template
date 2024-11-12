0b08bc383717d7f6fde0c3fd79ed4494363ded25
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-11-11 22:47:05 -0800

    update: remove hello API endpoint

--- packages/client/src/routes/root.tsx
+++ packages/client/src/routes/root.tsx
@@ -1,25 +1,10 @@
-import { type ReactNode, useEffect, useState } from 'react';
+import { type ReactNode } from 'react';
 import { client } from '@/api/client';
 import { SolidButton } from '@/components/buttons/solid-button';
 import { Link } from '@/components/link';
 
 export function Root(): ReactNode {
-    const [message, setMessage] = useState('');
-
-    useEffect(() => {
-        async function getMessage() {
-            const api = client();
-            const data: { message: string } = await api.get('api/hello').json();
-
-            setMessage(data.message);
-        }
-
-        void getMessage().catch(() => console.log('error in server'));
-    }, []);
-
     const getUsers = async () => {
-        console.log('get users');
-
         const api = client();
 
         try {
@@ -42,12 +27,6 @@ export function Root(): ReactNode {
         <div className="min-h-screen flex flex-col gap-4">
             <div className="flex flex-col items-center">
                 <h1 className="text-4xl font-semibold">Express + React Template</h1>
-            </div>
-            <div className="flex justify-center">
-                <div className="border px-4 py-3 rounded">
-                    <span>From Server: </span>
-                    <code className="bg-gray-200 px-2 py-1 rounded">{message}</code>
-                </div>
             </div>
             <Link to="/register" variant="solid-button">
                 Sign Up
--- packages/server/src/routes/api.ts
+++ packages/server/src/routes/api.ts
@@ -4,9 +4,3 @@ import { authController } from '@/http/controllers/auth-controller';
 export const apiRouter = Router();
 
 apiRouter.use('/auth', authController);
-
-apiRouter.get('/hello', async (_req, res) => {
-    res.status(200).json({
-        message: 'hello world',
-    });
-});
