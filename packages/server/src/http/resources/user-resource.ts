f40214c51a9aa8d70f5a83a620a36a4d472ce569
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-11-22 18:32:12 -0800

    update(server): convert Date to string in user-resource

--- packages/server/src/http/resources/user-resource.ts
+++ packages/server/src/http/resources/user-resource.ts
@@ -8,9 +8,9 @@ export class UserResource extends Resource<UserSelect> {
             first_name: user.first_name,
             id: user.ulid,
             last_name: user.last_name,
-            last_signed_in_at: user.last_signed_in_at,
-            created_at: user.created_at,
-            updated_at: user.updated_at,
+            last_signed_in_at: user.last_signed_in_at?.toISOString() ?? null,
+            created_at: user.created_at.toISOString(),
+            updated_at: user.updated_at.toISOString(),
         });
 
         return this;
