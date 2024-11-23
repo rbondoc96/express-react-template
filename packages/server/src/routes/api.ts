b360e0fecce05c98b80fedb2d88f2edea5f62180
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-11-23 03:14:07 -0800

    feat(server): add Swagger docs

--- packages/server/src/routes/api.ts
+++ packages/server/src/routes/api.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
 import { authController } from '@/http/controllers/auth-controller';
+import swaggerDocument from '@/swagger.json';
 
 export const apiRouter = Router();
 
 apiRouter.use('/auth', authController);
+apiRouter.use('/docs', swaggerUi.serve);
+apiRouter.get('/docs', swaggerUi.setup(swaggerDocument));
