881de080f986f20411fa5f35632301c3be45c7b8
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-12-04 19:40:32 -0800

    chore(server): add time to log request middleware

--- packages/server/src/http/middlewares/log-request-middleware.ts
+++ packages/server/src/http/middlewares/log-request-middleware.ts
@@ -1,10 +1,12 @@
 import { type RequestHandler } from 'express';
+import { DateTime } from 'luxon';
 
 export const logRequestMiddleware: RequestHandler = async (req, _res, next) => {
     const requestMethod = req.method;
     const requestPath = req.path;
+    const nowFormatted = DateTime.now().toLocaleString(DateTime.TIME_24_WITH_SECONDS);
 
-    console.log(`[REQUEST]: ${requestMethod} ${requestPath}`);
+    console.log(`[REQUEST ${nowFormatted}]: ${requestMethod} ${requestPath}`);
 
     next();
 };
