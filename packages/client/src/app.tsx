f72e9e7c7ef301632f493f5cf245d3f2027dfa64
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-12-04 19:23:31 -0800

    refactor(client): move QueryClient definition out of dedicated file

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,8 +1,16 @@
-import { QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { type ReactNode } from 'react';
 import { RouterProvider } from 'react-router-dom';
-import { queryClient } from '@/queryClient';
 import { router } from '@/routes/_router';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: false,
+            staleTime: 30_000,
+        },
+    },
+});
 
 export function App(): ReactNode {
     return (
