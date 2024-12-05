848b307f409194eae4d9b654caf14ffbd3b205ad
Rodrigo Bondoc <rbondoc96@gmail.com>
2024-12-04 21:52:16 -0800

    update(client): redesign landing page

--- packages/client/src/routes/root.tsx
+++ packages/client/src/routes/root.tsx
@@ -1,25 +1,30 @@
-import { type ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { Link } from '@/components/ui/link';
 
 export function Root(): ReactNode {
     return (
-        <div className="container flex flex-col justify-between px-8">
-            <header className="my-6">
+        <div className="flex flex-col">
+            <header className="container py-5 border-b">
                 <div className="flex justify-between">
-                    <Link to="/">Home</Link>
+                    <Link className="text-sm" to="/">
+                        Home
+                    </Link>
+                    <Link className="text-sm" to="/login">
+                        Log in
+                    </Link>
                 </div>
             </header>
-            <main>
-                <div className="min-h-screen flex flex-col gap-4">
-                    <div className="flex flex-col items-center">
-                        <h1 className="text-4xl font-semibold">Express + React Template</h1>
+            <main className="container flex flex-col mt-19">
+                <div className="min-h-screen flex flex-col gap-8">
+                    <div className="flex flex-col items-center gap-8">
+                        <h1 className="text-5xl font-bold">Express + React Template</h1>
+                        <p className="text-xl">A template for projects written with React and Express.</p>
                     </div>
-                    <Link to="/register" variant="solid-button">
-                        Sign Up
-                    </Link>
-                    <Link to="/login" variant="solid-button">
-                        Login
-                    </Link>
+                    <div className="flex justify-center">
+                        <Link to="/register" variant="solid-button">
+                            Get started
+                        </Link>
+                    </div>
                 </div>
             </main>
         </div>
